a8e91903a4e0d6ee547f68e92f2e30d953083aa3
Lucas Farias <lucas.farias@rtbhouse.com>
2024-10-28 18:56:12 -0300

    create controller create-account

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,31 @@
+import { Body, ConflictException, Controller, Post } from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('user with same email address already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
