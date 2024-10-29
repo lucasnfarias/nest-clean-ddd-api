ce84b07c905ffd1953dbc50e61e2f96c8636374a
Lucas Farias <lucas.farias@rtbhouse.com>
2024-10-29 20:10:01 -0300

    create auth strategy and guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from 'src/auth/auth.module'
+import { AuthenticateController } from 'src/controllers/authenticate.controller'
 import { CreateAccountController } from 'src/controllers/create-account.controller'
+import { CreateQuestionController } from 'src/controllers/create-question.controller'
 import { envSchema } from 'src/env'
 import { PrismaService } from 'src/prisma/prisma.service'
 
@@ -10,8 +13,13 @@ import { PrismaService } from 'src/prisma/prisma.service'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle() {
+    return 'ok'
+  }
+}
