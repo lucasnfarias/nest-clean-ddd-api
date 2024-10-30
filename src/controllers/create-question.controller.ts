b00ff8341fad5b16bdb3be163eec18f79f53deee
Lucas Farias <lucas.farias@rtbhouse.com>
2024-10-30 19:24:07 -0300

    create current user decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { UserPayload } from 'src/auth/jwt.strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
@@ -17,8 +18,8 @@ export class CreateQuestionController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Post()
-  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle() {
-    return 'ok'
+  // @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle(@CurrentUser() user: UserPayload) {
+    return user
   }
 }
