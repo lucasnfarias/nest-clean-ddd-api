e9f6c4ba0b140c92726609c5f1ce3f2c235ebd29
Lucas Farias <lucas.farias@rtbhouse.com>
2024-11-02 17:05:47 -0300

    implement rest of methods of prisma questions repository

--- src/infra/database/prisma/mappers/prisma-question.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question.mapper.ts
@@ -1,7 +1,7 @@
 import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Prisma, Question as PrismaQuestion } from '@prisma/client'
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
@@ -20,5 +20,17 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      title: question.title,
+      content: question.content,
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
