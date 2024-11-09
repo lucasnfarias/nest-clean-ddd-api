8fabadd74d36634933a2a2384cc99e4cc145bd19
Lucas Farias <lucas.farias@rtbhouse.com>
2024-11-09 18:20:34 -0300

    create comment-on-answer controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,7 @@
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
 import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-questions'
 import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
@@ -17,6 +18,7 @@ import { DatabaseModule } from '@/infra/database/database.module'
 import { AnswerQuestionController } from '@/infra/http/controllers/answer-question.controller'
 import { AuthenticateController } from '@/infra/http/controllers/authenticate.controller'
 import { ChooseQuestionBestAnswerController } from '@/infra/http/controllers/choose-question-best-answer.controller'
+import { CommentOnAnswerController } from '@/infra/http/controllers/comment-on-answer.controller'
 import { CommentOnQuestionController } from '@/infra/http/controllers/comment-on-question.controller'
 import { CreateAccountController } from '@/infra/http/controllers/create-account.controller'
 import { CreateQuestionController } from '@/infra/http/controllers/create-question.controller'
@@ -47,6 +49,7 @@ import { Module } from '@nestjs/common'
     ChooseQuestionBestAnswerController,
     CommentOnQuestionController,
     DeleteQuestionCommentController,
+    CommentOnAnswerController,
   ],
   providers: [
     RegisterStudentUseCase,
@@ -63,6 +66,7 @@ import { Module } from '@nestjs/common'
     ChooseQuestionBestAnswerUseCase,
     CommentOnQuestionUseCase,
     DeleteQuestionCommentUseCase,
+    CommentOnAnswerUseCase,
   ],
 })
 export class HttpModule {}
