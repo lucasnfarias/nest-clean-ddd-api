2660859979129298bd9ce5db844434a88875b99e
Lucas Farias <lucas.farias@rtbhouse.com>
2024-11-09 18:00:16 -0300

    create comment-on-question controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,7 @@
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-questions'
 import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
@@ -13,6 +15,8 @@ import { CryptographyModule } from '@/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { AnswerQuestionController } from '@/infra/http/controllers/answer-question.controller'
 import { AuthenticateController } from '@/infra/http/controllers/authenticate.controller'
+import { ChooseQuestionBestAnswerController } from '@/infra/http/controllers/choose-question-best-answer.controller'
+import { CommentOnQuestionController } from '@/infra/http/controllers/comment-on-question.controller'
 import { CreateAccountController } from '@/infra/http/controllers/create-account.controller'
 import { CreateQuestionController } from '@/infra/http/controllers/create-question.controller'
 import { DeleteAnswerController } from '@/infra/http/controllers/delete-answer.controller'
@@ -38,6 +42,8 @@ import { Module } from '@nestjs/common'
     EditAnswerController,
     DeleteAnswerController,
     FetchQuestionAnswersController,
+    ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
   ],
   providers: [
     RegisterStudentUseCase,
@@ -51,6 +57,8 @@ import { Module } from '@nestjs/common'
     EditAnswerUseCase,
     DeleteAnswerUseCase,
     FetchQuestionAnswersUseCase,
+    ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
   ],
 })
 export class HttpModule {}
