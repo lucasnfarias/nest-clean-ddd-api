332c12e9237f5880985c4ac99ad526c7d55737a4
Lucas Farias <lucas.farias@rtbhouse.com>
2024-11-10 12:21:41 -0300

    implement r2/aws storage module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -16,6 +16,7 @@ import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cas
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
 import { CryptographyModule } from '@/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { AnswerQuestionController } from '@/infra/http/controllers/answer-question.controller'
@@ -37,10 +38,11 @@ import { FetchQuestionCommentsController } from '@/infra/http/controllers/fetch-
 import { FetchRecentQuestionsController } from '@/infra/http/controllers/fetch-recent-questions.controller'
 import { GetQuestionBySlugController } from '@/infra/http/controllers/get-question-by-slug.controller'
 import { UploadAttachmentController } from '@/infra/http/controllers/upload-attachment.controller'
+import { StorageModule } from '@/infra/storage/storage.module'
 import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [DatabaseModule, CryptographyModule],
+  imports: [DatabaseModule, CryptographyModule, StorageModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
@@ -81,6 +83,7 @@ import { Module } from '@nestjs/common'
     DeleteAnswerCommentUseCase,
     FetchQuestionCommentsUseCase,
     FetchAnswerCommentsUseCase,
+    UploadAndCreateAttachmentUseCase,
   ],
 })
 export class HttpModule {}
